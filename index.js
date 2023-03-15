a8b16318aba07e76000add65a01a3b391e5ac164
Dalton Knuckles <djck244@gmail.com>
2023-03-15 15:54:50 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 15.5;
+let num2 = 4
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() + 1);
+let num3 = 11;
+let num4 = 7;
+let mod = num3 % num4;
+let numList = (1, 15, 14, 18, 17, 20);
+let max = Math.max(numList);
